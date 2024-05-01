cdd91d7254f5a17f6cb7d81eabf3f1a79798aec4
Hovhannes <shaxulyan87@gmail.com>
2024-05-01 12:33:50 +0400

    Update package start

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var app = express();
 app.use(cors());
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
